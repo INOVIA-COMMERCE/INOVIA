a5b022c2bc0ad49c4b68111d8fd9339b1c0148f9
INOVIA-COMMERCE <tsakemsocrate48@gmail.com>
2025-02-07 09:14:18 +0100

    Update paiements.js

--- paiements.js
+++ paiements.js
@@ -24,7 +24,43 @@ document.addEventListener("DOMContentLoaded", function () {
             customer_state: "CM",
             customer_zip_code: "06510",
         });
+document.getElementById("btn-payer").addEventListener("click", function () {
+    CinetPay.setConfig({
+        apikey: '68265673267a5ae17cf5a08.79292594', // Remplace par ta vraie API Key
+        site_id: 105887404, // Remplace par ton vrai Site ID
+        notify_url: 'https://mondomaine.com/notify/',
+        mode: 'PRODUCTION'
+    });
 
+    CinetPay.getCheckout({
+        transaction_id: Math.floor(Math.random() * 100000000).toString(),
+        amount: 100,
+        currency: 'XOF',
+        channels: 'ALL',
+        description: 'Test de paiement',
+        customer_name: "Joe",
+        customer_surname: "Down",
+        customer_email: "down@test.com",
+        customer_phone_number: "088767611",
+        customer_address: "BP 0024",
+        customer_city: "Antananarivo",
+        customer_country: "CM",
+        customer_state: "CM",
+        customer_zip_code: "06510",
+    });
+
+    CinetPay.waitResponse(function (data) {
+        if (data.status == "REFUSED") {
+            alert("Votre paiement a échoué");
+        } else if (data.status == "ACCEPTED") {
+            alert("Votre paiement a été effectué avec succès");
+        }
+    });
+
+    CinetPay.onError(function (data) {
+        console.log(data);
+    });
+});
         CinetPay.waitResponse(function (data) {
             if (data.status == "REFUSED") {
                 alert("Votre paiement a échoué");
